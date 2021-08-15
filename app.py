0090303681ec24fb99dd38b248ccdfecec131db5
Arthur Caron <arthurcaron@me.com>
2021-08-15 16:30:35 +0200

    code ready to be deployed to production

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import datetime
 import requests
 import pandas as pd
+import pydeck as pdk
 
 from streamlit.state.session_state import Value
 
@@ -24,24 +25,28 @@ pickuptime = st.sidebar.time_input("insert a pickup time")
 '''
 - pickup longitude
 '''
-p_longitude = st.sidebar.number_input('Insert the pickup longitude')
+p_longitude = st.sidebar.number_input('Insert the pickup longitude', value = -73.9385)
 
 '''
 - pickup latitude
 '''
-p_latitude = st.sidebar.number_input('Insert the pickup latitude')
+p_latitude = st.sidebar.number_input('Insert the pickup latitude', value= 40.6643)
 '''
 - dropoff longitude
 '''
-d_longitude = st.sidebar.number_input('Insert the dropoff longitude')
+d_longitude = st.sidebar.number_input('Insert the dropoff longitude', value = -73.9385)
 '''
 - dropoff latitude
 '''
-d_latitude = st.sidebar.number_input('Insert the dropoff latitude')
+d_latitude = st.sidebar.number_input('Insert the dropoff latitude', value= 40.6643)
 '''
 - passenger count
 '''
+"""
+## Please find below the map of New York
 
+In blue you have the pickup location and in red the dropoff one
+"""
 p_count = st.sidebar.number_input('Insert the passenger count', step = 1)
 
 pickupdatetime = str(pickupdate) + " " +str(pickuptime)
@@ -58,10 +63,43 @@ url = f'https://taxifare.lewagon.ai/predict?pickup_datetime={pickupdatetime}&pic
 
 response = requests.get(url)
 
-if p_longitude == 0 and p_latitude == 0 and d_longitude == 0 and d_latitude == 0:
+if response is None:
     prediction = 0
     st.markdown("# please fill in the parameters")
 else:
     prediction = round(response.json()['prediction'], 2)
-    st.markdown(f'# the prediction of the taxe fare price is {prediction} dollars ! ')
+    dict_map = {'lat': [p_latitude, d_latitude], 'lon' : [p_longitude, d_longitude]}
+    df = pd.DataFrame(dict_map)
+    st.pydeck_chart(pdk.Deck(
+        map_style='mapbox://styles/mapbox/light-v9',
+        initial_view_state=pdk.ViewState(
+         latitude=40.6643,
+         longitude=-73.9385,
+         zoom=11,
+         pitch=50,
+     ),
+        layers=[
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=df[0:1],
+             get_position='[lon, lat]',
+             get_color='[0, 96, 255, 160]',
+             get_radius=200,
+         ),
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=df[1:2],
+             get_position='[lon, lat]',
+             get_color='[200, 30, 0, 160]',
+             get_radius=200,
+         ),
+     ],
+    ))
+    if prediction > 0:
+        prediction = round(response.json()['prediction'], 2)
+        st.markdown(f'# the prediction of the taxe fare price is {prediction} dollars ! ')
+    else:
+        st.markdown("# please fill in the parameters")
+        
+
     
